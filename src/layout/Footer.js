1ff9cc3669402357bdae5d7a79ef0dfcde50a5a6
Komron <example@gmail.com>
2022-01-31 12:42:05 +0500

    the end

---
+++ src/layout/Footer.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <footer class="page-footer">
+      <div class="footer-copyright">
+        <div class="container">
+          © {new Date().getFullYear()} Copyright Text
+          <a class="grey-text text-lighten-4 right" href="#!">
+            More Links
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
